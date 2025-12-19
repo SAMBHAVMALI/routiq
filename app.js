beacb79b1db2148c0f41a89d20c5220f1d1bd180
SAMBHAVMALI <sambmali1234@gmail.com>
2025-12-20 04:06:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,10 @@ render();
 
 /* REGISTER SERVICE WORKER */
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/Routiq/service-worker.js");
-
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js", { scope: "./" })
+      .then(() => console.log("SW registered"))
+      .catch(err => console.error("SW error", err));
+  });
 }
